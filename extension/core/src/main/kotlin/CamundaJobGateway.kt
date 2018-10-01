31eca2577161953bc0bc4dadbe174828a2095951
Jan Galinski <jan.galinski@holisticon.de>
2018-10-01 18:00:55 +0200

    refactor cmd creation

--- extension/core/src/main/kotlin/CamundaJobGateway.kt
+++ extension/core/src/main/kotlin/CamundaJobGateway.kt
@@ -1,43 +1,45 @@
 package io.holunda.ext.customjob
 
-import io.holunda.ext.customjob.api.CreateJobCommand
-import io.holunda.ext.customjob.api.CreateJobGateway
-import io.holunda.ext.customjob.api.InsertJobCommand
-import io.holunda.ext.customjob.api.ScheduleJobCommand
+import io.holunda.ext.customjob.api.*
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl
 import org.camunda.bpm.engine.impl.context.Context
 import org.camunda.bpm.engine.impl.interceptor.Command
 import org.camunda.bpm.engine.impl.persistence.entity.MessageEntity
 import org.camunda.bpm.engine.impl.persistence.entity.TimerEntity
-import java.time.Instant
 import java.util.*
 
 class CamundaJobGateway(private val configuration: ProcessEngineConfigurationImpl, private val jobHandlerBuilder: CustomJobHandlerBuilder) : CreateJobGateway {
 
-  override fun send(cmd: CreateJobCommand): String {
-    val command = when (cmd) {
-      is ScheduleJobCommand -> Command<String> {
-        TimerEntity().apply {
-          jobHandlerType = cmd.jobHandlerType
-          duedate = Date.from(cmd.dueDate)
-          jobHandlerConfiguration = jobHandlerBuilder.jobHandlerConfiguration(cmd.payload)
+  override fun send(cmd: CreateJobCommand): String = execute(when (cmd) {
+    is ScheduleJobCommand -> toCamundaCommand(cmd)
+    is InsertJobCommand -> toCamundaCommand(cmd)
+  })
 
-          it.jobManager.schedule(this)
-        }.id
-      }
+  private fun JobPayload.toConfig() = jobHandlerBuilder.jobHandlerConfiguration(this)
 
-      is InsertJobCommand -> Command {
-        MessageEntity().apply {
-          jobHandlerType = cmd.jobHandlerType
-          jobHandlerConfiguration = jobHandlerBuilder.jobHandlerConfiguration(cmd.payload)
-
-          it.jobManager.insertJob(this)
-        }.id
-      }
+  private fun toCamundaCommand(cmd: ScheduleJobCommand) = Command {
+    val job = TimerEntity().apply {
+      jobHandlerType = cmd.jobHandlerType
+      duedate = Date.from(cmd.dueDate)
+      jobHandlerConfiguration = cmd.payload.toConfig()
     }
 
-    return execute(command)
+    it.jobManager.schedule(job)
+
+    return@Command job.id
   }
+
+  private fun toCamundaCommand(cmd: InsertJobCommand) = Command {
+    val job = MessageEntity().apply {
+      jobHandlerType = cmd.jobHandlerType
+      jobHandlerConfiguration = cmd.payload.toConfig()
+    }
+    it.jobManager.insertJob(job)
+
+    return@Command job.id
+  }
+
+
 
   private fun execute(command: Command<String>): String =
     if (Context.getCommandContext() == null)
