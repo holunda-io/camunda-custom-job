53597cc6de47b475c46c7fc215588392199595bf
Jan Galinski <jan.galinski@holisticon.de>
2021-02-13 11:00:20 +0100

    switch to maven, see #6, wip

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "camunda-custom-job"
-
-include("extension:api")
-findProject(":extension:api")?.name = "camunda-custom-job-api"
-
-
-include("extension:core")
-findProject(":extension:core")?.name = "camunda-custom-job"
